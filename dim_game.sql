32d242b9c6b37741931e7a0198367229a7dbb690
LapasaranJoemar <joemar.tarranco.lapasaran@gmail.com>
2024-10-19 20:24:47 +0800

    updated sql files

--- dim_game.sql
+++ dim_game.sql
@@ -1,19 +1,19 @@
 CREATE TABLE dim_game (
     app_id INT PRIMARY KEY,
-    name TEXT,
+    name VARCHAR(255),
     release_date DATE,
     required_age INT,
-    genres TEXT,
-    categories TEXT,
+    genres VARCHAR(255),
+    categories VARCHAR(255),
     tags TEXT,
     about_the_game LONGTEXT,
     header_image TEXT,
     website TEXT,
     support_url TEXT,
-    support_email TEXT,
+    support_email VARCHAR(255),
     achievements TEXT,
     recommendations TEXT,
     notes TEXT
 );
 
-SELECT * FROM dim_game;
+
